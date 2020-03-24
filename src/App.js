048e626662466c8b58c58c986bd72e37b1653fc0
Marina Baskova <marinabaskova24@gmail.com>
2020-03-23 23:52:26 -0400

    test for app fetches missions data and render data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
-
 import { fetchMissions } from './api/fetchMissions';
-
 import MissionForm from './components/MissionForm';
 import MissionsList from './components/MissionsList';
 
